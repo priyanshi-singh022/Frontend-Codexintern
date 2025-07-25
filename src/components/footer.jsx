525af7d7ac14e2dd0ec30944cba5a36a3f3285d7
pikupika <peeyuvi2002@gmail.com>
2025-07-26 01:11:23 +0530

    updated font color

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,32 +3,32 @@ import { NavLink } from "react-router-dom";
 
 function Footer() {
     return (
-        <footer class="bg-gray-900 pt-10 pb-10 px-10 tracking-wide">
-            <div class="max-w-screen-xl mx-auto">
-                <div class="grid grid-cols-2 lg:grid-cols-2 gap-4">
-                    <div class="lg:flex lg:items-center flex-col">
+        <footer className="bg-gray-900 pt-10 pb-10 px-10 tracking-wide">
+            <div className="max-w-screen-xl mx-auto">
+                <div className="grid grid-cols-2 lg:grid-cols-2 gap-4">
+                    <div className="lg:flex lg:items-center flex-col">
                         <a href="javascript:void(0)" className="p-5">
-                            <h1 class="text-white">CodeXintern Frontend Tasks</h1>
+                            <h1 className="text-white">CodeXintern Frontend Tasks</h1>
                         </a>
-                        <p class="text-slate-400 text-sm">© ReadymadeUI. All rights reserved for header, footer and features.
+                        <p className="text-slate-400 text-sm">© ReadymadeUI. All rights reserved for header, footer and features.
                         </p>
-                        <p className="text-slate-400 text-sm" ><a href="https://iconscout.com/icons/menu" class="text-underline-400 font-size-sm" target="_blank">Menu</a> by <a href="https://iconscout.com/contributors/unicons" class="text-underline font-size-sm">Unicons Font</a> on <a href="https://iconscout.com" class="text-underline font-size-sm">IconScout</a></p>
+                        <p className="text-slate-400 text-sm" ><a href="https://iconscout.com/icons/menu" className="text-underline-400 font-size-sm" target="_blank">Menu</a> by <a href="https://iconscout.com/contributors/unicons" className="text-underline font-size-sm">Unicons Font</a> on <a href="https://iconscout.com" className="text-underline font-size-sm">IconScout</a></p>
                     </div>
 
                     <div>
-                        <h4 class="text-base mb-4 text-white">Go to page:</h4>
-                        <ul class="space-y-4">
+                        <h4 className="text-base mb-4 text-white">Go to page:</h4>
+                        <ul className="space-y-4">
                             <li>
-                                <NavLink to={"/"} href="javascript:void(0)" class="text-slate-400 hover:text-white text-sm">Translator</NavLink>
+                                <NavLink to={"/"} href="javascript:void(0)" className="text-slate-200 hover:text-white text-sm">Translator</NavLink>
                             </li>
                             <li>
-                                <NavLink to={"/textgen"} href="javascript:void(0)" class="text-slate-400 hover:text-white text-sm">String Generator</NavLink>
+                                <NavLink to={"/textgen"} href="javascript:void(0)" className="text-slate-200 hover:text-white text-sm">String Generator</NavLink>
                             </li>
                             <li>
-                                <NavLink to={"/feature"} href="javascript:void(0)" class="text-slate-400 hover:text-white text-sm">Features</NavLink>
+                                <NavLink to={"/feature"} href="javascript:void(0)" className="text-slate-200 hover:text-white text-sm">Features</NavLink>
                             </li>
                             <li>
-                                <NavLink to={"/about"} href="javascript:void(0)" class="text-slate-400 hover:text-white text-sm">About</NavLink>
+                                <NavLink to={"/about"} href="javascript:void(0)" className="text-slate-200 hover:text-white text-sm">About</NavLink>
                             </li>
                         </ul>
                     </div>
